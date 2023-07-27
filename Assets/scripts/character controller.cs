ea7238d1b50f37991be58ac900ee1352dbea1d75
luckyluck06 <140613029+luckyluck06@users.noreply.github.com>
2023-07-27 17:44:35 +0200

    code attack

--- Assets/scripts/character controller.cs
+++ Assets/scripts/character controller.cs
@@ -6,7 +6,8 @@ public class charactercontroller : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce = 10;
-
+    [SerializeField] private GameObject collider;
+    [SerializeField] private float attackDuration = 0.1f;
 
     private Rigidbody2D rb;
     private Animator animator;
@@ -14,7 +15,11 @@ public class charactercontroller : MonoBehaviour
     private int canJump;
     private int countJump;
 
-    
+    private bool oldAttack = false;
+    private float attackTime = 0.0f;
+    private float startScaleX = 2.5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,39 +27,60 @@ public class charactercontroller : MonoBehaviour
         animator = GetComponent<Animator>();
 
         sr = GetComponent<SpriteRenderer>();
+        startScaleX = transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        
-        float crtMove = Input.GetAxis("Horizontal")*speed;
+        float crtMove = Input.GetAxis("Horizontal") * speed;
         rb.velocity = new Vector2(crtMove, rb.velocity.y);
         animator.SetFloat("speed", crtMove);
 
         bool crtJump = Input.GetButtonDown("Jump") && canJump > 0;
-        if(crtJump)
+        if (crtJump)
         {
             Debug.Log("Jump");
-            rb.velocity=new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             countJump++;
             canJump--;
         }
 
-        sr.flipX = rb.velocity.x > 0;
-    }
-    private void OnTriggerEnter2D(Collider2D other)
 
-    {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        { 
-            canJump=2;
+        Vector3 scale = transform.localScale;
+        float flipFactor = rb.velocity.x > 0 ? -1 : 1;
+        scale.x = flipFactor * startScaleX;
+        transform.localScale = scale;
+
+
+        if (Input.GetButtonDown("Fire2") && !oldAttack && attackTime <= 0)
+        {
+            animator.SetBool("Attack", true);
+            collider.SetActive(true);
+            attackTime = attackDuration;
         }
 
+        if (attackTime <= 0)
+        {
+            collider.SetActive(false);
+            animator.SetBool("Attack", false);
+        }
 
+        attackTime -= Time.deltaTime;
+        oldAttack = Input.GetButtonDown("Fire2");
+    }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            canJump = 2;
+        }
 
+        if (other.TryGetComponent(out EnnemiPatrol enemy))
+        {
+            Debug.Log("Tet");
+        }
     }
 
 }
